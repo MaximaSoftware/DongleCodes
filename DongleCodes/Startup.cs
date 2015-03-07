59ecec81ec2bd7b160381f87cef7e1097236712d
Mark Pearl <markpearl@gmail.com>
2015-03-07 11:03:55 +0200

    Initial

---
+++ DongleCodes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DongleCodes.Startup))]
+namespace DongleCodes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
